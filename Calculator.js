a7a473160f377300073735e1f966fdf53275b6a7
jagadeeshwaranS21 <jagadeeshwaran721@gmail.com>
2024-03-02 16:07:06 +0530

    added Calcuator project

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input
+}
+
+function Calculate(){
+    try{
+    display.value = eval(display.value)
+    }
+    catch(e){
+        display.value="Error"
+    }
+}
+
+function ClearDisplay(){
+    display.value=""
+}
